f624681622274a43581f905bd21c30d4a9f3b4f5
Pranart Dechpokket <jojobarcream@gmail.com>
2020-06-27 14:08:12 +0700

    fix

---
+++ FormStandard/Converter/CollectionHasElementsConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+
+namespace FormStandard
+{
+    public class CollectionHasElementsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return false;
+
+            return ((IEnumerable<object>)value).Any();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+
+    }
+}
---
+++ FormStandard/Converter/IsNotHaveVoidConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace FormStandard.Converter
+{
+    public class IsNotHaveVoidConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !((value as string) ?? "void").Contains("void");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
